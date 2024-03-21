fb464a917127ca95f77edcd6affd91ae3bdbcbf5
Mohamed Bengrich <mohamed@etamin.studio>
2024-03-22 00:40:59 +0100

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_21_232657) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "clients", force: :cascade do |t|
     t.string "nom_complet"
     t.string "adresse"
@@ -64,34 +58,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_21_232657) do
     t.index ["facture_id"], name: "index_items_on_facture_id"
   end
 
-  create_table "options", force: :cascade do |t|
-    t.bigint "category_id", null: false
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_options_on_category_id"
-  end
-
-  create_table "product_options", force: :cascade do |t|
-    t.bigint "product_id", null: false
-    t.bigint "option_id", null: false
-    t.boolean "is_paid", default: false
-    t.integer "price_cents"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["option_id"], name: "index_product_options_on_option_id"
-    t.index ["product_id"], name: "index_product_options_on_product_id"
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description", null: false
-    t.integer "price_cents", null: false
-    t.boolean "available", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -111,7 +77,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_21_232657) do
   add_foreign_key "factures", "clients"
   add_foreign_key "items", "devis"
   add_foreign_key "items", "factures"
-  add_foreign_key "options", "categories"
-  add_foreign_key "product_options", "options"
-  add_foreign_key "product_options", "products"
 end
